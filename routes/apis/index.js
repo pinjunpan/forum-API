f66a064c8741567d218d1d6b6602761149a44f79
pinjunpan <pinjunpann@gmail.com>
2024-12-11 17:06:48 +0800

    feat: add api error handler & delete restaurant api

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -5,8 +5,12 @@ const admin = require('./modules/admin')
 
 const restController = require('../../controllers/apis/restaurant-controller')
 
+const { apiErrorHandler } = require('../../middleware/error-handler')
+
 router.use('/admin', admin)
 
 router.get('/restaurants', restController.getRestaurants)
+
+router.use('/', apiErrorHandler)
 
 module.exports = router
